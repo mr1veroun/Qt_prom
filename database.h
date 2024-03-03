bc546d8757c52d2baf26e2d64c975cc8dfb99649
slavechief <uvaysov.2005@mail.ru>
2024-03-03 12:15:54 +0300

    DBFilesSeparating

--- database.h
+++ database.h
@@ -7,6 +7,7 @@
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlRecord>
 
+
 class Database
 {
 
@@ -14,61 +15,24 @@ private:
 
     static Database * p_instance;
     QSqlDatabase m_database;
-protected:
-
 
 protected:
     Database(){}
     Database(const Database& );
     Database& operator = (Database&);
-    ~Database() {
-        if (m_database.isOpen())
-            disconnect();
-        if (p_instance)
-            delete p_instance;
-    }
+    ~Database();
 
 public:
+    static Database* getInstance();
+
+    QSqlQuery send_query(QString queryStr);
+
+    bool connect();
 
 
-    static Database* getInstance(){
-        if (!p_instance)
-        {
-            p_instance = new Database();
-        }
-        return p_instance;
-    }
-
-    QSqlQuery send_query(QString queryStr)
-    {
-        QSqlQuery query;
-        query.prepare(queryStr);
-        if (!query.exec())
-            qDebug() << "Invalid query:" << query.lastError().text();;
-        return query;
-    }
-    bool connect()
-    {
-        // Устанавливаем соединение с базой данных
-        m_database = QSqlDatabase::addDatabase("QSQLITE");
-        m_database.setDatabaseName("C:/Users/Magras/Documents/Qt_prom/sql_app.db");
-        if (!m_database.open())
-        {
-            qDebug() << "Ошибка соединения с базой данных";
-            return false;
-        }
-
-        return true;
-    }
-
-    void disconnect()
-    {
-        // Отключаемся от базы данных
-        m_database.close();
-    }
+    void disconnect();
 
 
 };
-
 
 #endif // DATABASE_H
--- echoServer.pro
+++ echoServer.pro
@@ -20,6 +20,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    database.cpp \
     main.cpp \
     mytcpserver.cpp \
     functionserver.cpp \
